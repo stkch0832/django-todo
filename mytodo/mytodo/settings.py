7e7ccc497d2045720cdbfde763b17dc3cafe92c2
stkch0832 <stkch0832@gmail.com>
2022-07-12 12:25:56 +0900

    fix: a typo
    Because the migration resulted in an error

--- mytodo/mytodo/settings.py
+++ mytodo/mytodo/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'widget_tweaks',
-    'app.apps.appConfig',
+    'app.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
