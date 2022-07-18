a355a723b08d4acd0809b766b54fa24ef7aa1ae1
stkch0832 <stkch0832@gmail.com>
2022-07-18 18:10:33 +0900

    Add: URLconf ticket_detail

--- mytodo/app/urls.py
+++ mytodo/app/urls.py
@@ -3,4 +3,5 @@ from app import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('ticket/<int:pk>/', views.TicketDetailView.as_view(), name='ticket_detail'),
 ]
