dd31cc060271296efb95a99a02606d8d406eaa53
stkch0832 <stkch0832@gmail.com>
2022-09-03 21:04:48 +0900

    Add: Model Post

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField("タスク名", max_length=20)
+    status = models.IntegerField("ステータス")
+    priority = models.IntegerField("優先度")
+    limit_datetime = models.DateTimeField("期限")
+    description = models.TextField("詳細")
+
+    created_at = models.DateTimeField("作成日時", auto_now_add=True)
+    updated_at = models.DateTimeField("更新日時", auto_now=True)
+
+    def __str__(self):
+        return self.title
