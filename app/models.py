12a0e08195dc362f4c795734ff415fe91a0b82ed
stkch0832 <stkch0832@gmail.com>
2022-09-06 22:37:33 +0900

    Add: Dropdown options
    To show unselected.

--- app/models.py
+++ app/models.py
@@ -1,11 +1,27 @@
 from django.conf import settings
 from django.db import models
 
+POST_STATUS_CHOICES = (
+            (1, '未着手'),
+            (2, '進行中'),
+            (3, '保留'),
+            (4, '完了'),
+            (5, '中止'),
+        )
+
+POST_PRIORITY_CHOICES = (
+            (1, '緊急・重要'),
+            (2, '緊急'),
+            (3, '重要'),
+            (4, '期限内'),
+            (5, '期限なし'),
+        )
+
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField("タスク名", max_length=20)
-    status = models.IntegerField("ステータス")
-    priority = models.IntegerField("優先度")
+    status = models.IntegerField("ステータス", choices=POST_STATUS_CHOICES)
+    priority = models.IntegerField("優先度", choices=POST_PRIORITY_CHOICES)
     limit_datetime = models.DateTimeField("期限")
     description = models.TextField("詳細")
 
