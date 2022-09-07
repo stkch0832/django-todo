a7bfa212fa2901dd8af5d2daec10dc753a5673d1
stkch0832 <stkch0832@gmail.com>
2022-09-07 20:46:58 +0900

    Add: PostEdit view

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new/', views.CreatePostView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', views.PostEditView.as_view(), name='post_edit'),
 ]
--- app/views.py
+++ app/views.py
@@ -45,3 +45,39 @@ class CreatePostView(View):
         return render(request, 'app/post_form.html', context = {
             'form': form,
         })
+
+
+class PostEditView(View):
+    def get(self, request, *args, **kwargs):
+        post_data = Post.objects.get(id=self.kwargs['pk'])
+        form = PostModelForm(
+            request.POST or None,
+            initial={
+                'title': post_data.title,
+                'status': post_data.status,
+                'priority': post_data.priority,
+                'limit_datetime': post_data.limit_datetime,
+                'description': post_data.description,
+            }
+        )
+
+        return render(request, 'app/post_form.html', context={
+            'form': form
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = PostModelForm(request.POST or None)
+
+        if form.is_valid():
+            post_data = Post.objects.get(id=self.kwargs['pk'])
+            post_data.title = form.cleaned_data['title']
+            post_data.status = form.cleaned_data['status']
+            post_data.priority = form.cleaned_data['priority']
+            post_data.limit_datetime = form.cleaned_data['limit_datetime']
+            post_data.description = form.cleaned_data['description']
+            post_data.save()
+            return redirect('post_detail', self.kwargs['pk'])
+
+        return render(request, 'app/post_form.hrml', context={
+            'form': form,
+        })
