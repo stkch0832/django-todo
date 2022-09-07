1e84f5586c958af31f0ee6db364bd5cb288608db
stkch0832 <stkch0832@gmail.com>
2022-09-07 20:27:01 +0900

    Add: CreatePost page, view and form

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from app import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.CreatePostView.as_view(), name='post_new'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostModelForm
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
@@ -15,4 +16,32 @@ class PostDetailView(View):
         post_data = Post.objects.get(id=self.kwargs['pk'])
         return render(request, 'app/post_detail.html', context= {
             'post_data': post_data,
+        })
+
+
+class CreatePostView(View):
+    def get(self, request, *args, **kwargs):
+        form = PostModelForm(request.POST or None)
+
+        return render(request, 'app/post_form.html', context = {
+            'form': form,
+            })
+
+    def post(self, request, *args, **kwargs):
+        form = (request.POST or None)
+
+        if form.is_valid():
+            post_data = Post()
+            post_data.user = request.user
+            post_data.title = form.cleaned_data['title']
+            post_data.status = form.cleaned_data['status']
+            post_data.priority = form.cleaned_data['priority']
+            post_data.limit_datetime = form.cleaned_data['limit_datetime']
+            post_data.description = form.cleaned_data['description']
+
+            post_data.save()
+            return redirect('post_detail', post_data.id)
+
+        return render(request, 'app/post_form.html', context = {
+            'form': form,
         })
